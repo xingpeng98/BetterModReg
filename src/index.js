83bda6691c4e5991488b1cedd2da5cae9c5993a1
urlydunknwme <67061972+urlydunknwme@users.noreply.github.com>
2022-03-24 19:02:59 +0800

    Login Page links to student_info.json

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
-import Homepage from './Homepage';
+import LoginPage from './LoginPage.js';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Homepage />
+    <LoginPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
