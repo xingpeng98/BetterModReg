8055c4db77e9194e8fb7993a313fea30aedefc9d
Marcusyeoce <e0310209@u.nus.edu>
2022-03-28 04:08:49 +0800

    added skeleton modules page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import Layout from "./components/Layout.js";
 import LoginPage from "./LoginPage.js";
 import ProfilePage from "./ProfilePage.js";
+import ModulesPage from "./ModulesPage.js";
 
 function App() {
   return (
@@ -14,13 +15,15 @@ function App() {
           <Container>
             <Nav className="me-auto">
               <Nav.Link href="/">Login</Nav.Link>
-              <Nav.Link href="/Profile">Profile</Nav.Link>
+              <Nav.Link href="/profile">Profile</Nav.Link>
+              <Nav.Link href="/modules">Modules</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
 
         <Routes>
           <Route exact path="/profile" element={<ProfilePage />} />
+          <Route exact path="/modules" element={<ModulesPage />} />
           <Route path="/" element={<LoginPage />} />
         </Routes>
       </Router>
---
+++ src/ModulesPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ModulesPage() {
+  return <h2>Modules Page</h2>;
+}
+
+export default ModulesPage;
