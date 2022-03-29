d2fb8eabc668353c2dc30d8b916cdee17a7fb8d0
Marcusyeoce <e0310209@u.nus.edu>
2022-03-30 00:50:53 +0800

    basic profile page

--- src/ModulesPage.js
+++ src/ModulesPage.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function ModulesPage() {
-  return <h2>Modules</h2>;
+  const modules = require("./module_info_soc.json");
+  console.log(modules);
+
+  return (
+    <div>
+      <h2>Modules</h2>
+      {/* {modules} */}
+      {modules.map((module, id) => {
+        <div>{module.course}</div>;
+      })}
+    </div>
+  );
 }
 
 export default ModulesPage;
--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -6,7 +6,18 @@ function ProfilePage() {
   return (
     <div>
       <h2>Profile</h2>
-      {user ? <p>{user.student.student_name}</p> : <p>Not logged in yet!</p>}
+      {user ? (
+        <div>
+          <h4>Name: {user.student.student_name}</h4>
+          <h4>Username: {user.student.username}</h4>
+          <h4>Seniority: {user.student.seniority}</h4>
+          <h4>Email: {user.student.email}</h4>
+          <h4>Major: {user.student.major}</h4>
+          <h4>Minor: {user.student.minor}</h4>
+        </div>
+      ) : (
+        <p>Not logged in yet!</p>
+      )}
     </div>
   );
 }
