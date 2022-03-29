87b14635db254905cd2408503afdbd90279d5297
Marcusyeoce <e0310209@u.nus.edu>
2022-03-30 01:02:59 +0800

    basic modules page

--- src/ModulesPage.js
+++ src/ModulesPage.js
@@ -1,15 +1,33 @@
 import React from "react";
 
 function ModulesPage() {
-  const modules = require("./module_info_soc.json");
+  const modules = require("./module_info_soc_short.json");
   console.log(modules);
+
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  const bidModule = () => {};
 
   return (
     <div>
       <h2>Modules</h2>
-      {/* {modules} */}
       {modules.map((module, id) => {
-        <div>{module.course}</div>;
+        return (
+          <div
+            style={{
+              borderStyle: "solid",
+            }}
+          >
+            <div>
+              <p>{module.title}</p>
+              <p>{module.moduleCode}</p>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <p>Vacancy: {module.vacancy}</p>
+                <button>Bid</button>
+              </div>
+            </div>
+          </div>
+        );
       })}
     </div>
   );
