548ea59f4134fa78be5442b46d7927173e1376cd
Marcusyeoce <e0310209@u.nus.edu>
2022-04-11 15:09:59 +0800

    admin flow working

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {ethers} from 'ethers';
+import { ethers } from "ethers";
 
 function ProfilePage() {
   const user = JSON.parse(localStorage.getItem("user"));
@@ -7,7 +7,7 @@ function ProfilePage() {
   const [defaultAccount, setDefaultAccount] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
   const [userBalance, setUserBalance] = useState(null);
-  const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+  const [connButtonText, setConnButtonText] = useState("Connect Wallet");
 
   //const keys = require("./keys.json");
   //const address = Object.keys(keys.addresses)[id];
@@ -17,31 +17,33 @@ function ProfilePage() {
 
   const connectWalletHandler = async () => {
     if (window.ethereum) {
-      window.ethereum.request({ method: 'eth_requestAccounts' })
-      .then(result => {
-        accountChangedHandler(result[0]);
-      })
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((result) => {
+          accountChangedHandler(result[0]);
+        });
     } else {
-      setErrorMessage('Install Metamask');
+      setErrorMessage("Install Metamask");
     }
-  }
+  };
 
   const accountChangedHandler = (newAccount) => {
     setDefaultAccount(newAccount);
     getUserBalance(newAccount);
-  }
+  };
 
   const getUserBalance = (address) => {
-    window.ethereum.request({method: 'eth_getBalance', params: [address, 'latest']})
-    .then(balance => {
-      setUserBalance(ethers.utils.formatEther(balance));
-    })
-  }
+    window.ethereum
+      .request({ method: "eth_getBalance", params: [address, "latest"] })
+      .then((balance) => {
+        setUserBalance(ethers.utils.formatEther(balance));
+      });
+  };
 
   return (
     <div>
       <h2>Profile</h2>
-      {user ? (
+      {user && user.id != 0 ? (
         <div>
           <h4>Name: {student.student_name}</h4>
           <h4>Username: {student.username}</h4>
@@ -54,10 +56,13 @@ function ProfilePage() {
           <h4>Address: {defaultAccount}</h4>
           <h4>Balance: {userBalance} </h4>
         </div>
-        
-      ) : (
-        <p>Not logged in yet!</p>
-      )}
+      ) : null}
+      {user && user.id == 0 ? (
+        <div>
+          <h4>You are admin</h4>
+        </div>
+      ) : null}
+      {!user ? <h4>Not logged in yet!</h4> : null}
       {errorMessage}
     </div>
   );
